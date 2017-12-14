78e3fc96b3dda0b023ca3d446bfabbb2360953a1
Leonid Prokopenko <just_beta@hotmail.com>
2017-12-14 17:04:09 +0200

    Done work
    added background player and player code

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	// The force which is added when the player jumps
+	// This can be changed in the Inspector window
+	public Vector2 jumpForce = new Vector2(0, 300);
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// Jump
+		if (Input.GetKeyUp("space"))
+		{
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(jumpForce);
+		}
+	}
+}
