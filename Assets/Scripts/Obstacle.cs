3837b6327737a35b95f68cc75a404261a6f40c4a
Leonid Prokopenko <just_beta@hotmail.com>
2018-02-08 19:49:21 +0200

    Added player and obstacle script

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+	public Vector2 velocity = new Vector2(-4, 0);
+
+	// Use this for initialization
+	void Start()
+	{
+		rigidbody2D.velocity = velocity;
+	}
+}
