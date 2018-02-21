3e5ac25ed17ed8fb751dac2e0843b8117a15ed35
Leonid Prokopenko <just_beta@hotmail.com>
2018-02-21 21:33:22 +0200

    added colders and score

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -5,13 +5,10 @@ using UnityEngine;
 public class Generate : MonoBehaviour {
 
 	public GameObject firePairs;
-
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating ("CreateObstacle", 1f, 1.5f);
 	}
-	
-	// Update is called once per frame
 	void CreateObstacle () {
 		Instantiate (firePairs);
 	}
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-	public Vector2 velocity = new Vector2(-4, 0); 
-
+	public Vector2 velocity = new Vector2(-4, 0);  
+	int score;
 	// Use this for initialization
 	void Start()
 	{
 		GetComponent<Rigidbody2D>().velocity = velocity;
+		transform.position = new Vector3(transform.position.x, transform.position.y -  Random.value, transform.position.z);
 	}
 
 	void Destroy()
@@ -20,6 +21,13 @@ public class Obstacle : MonoBehaviour
 	void OnBecameInvisible() {
 
 		Destroy();
+		score++;
 
 	}
+	// Update is called once per frame
+	void OnGUI () 
+	{
+		GUI.color = Color.black;
+		GUILayout.Label(" Score: " + score.ToString());
+	} 
 }
